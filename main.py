56009ae8fce37025dc33d6d1424f9b5f559bcc78
Livingston <livingstond96@gmail.com>
2026-01-17 02:01:07 +0530

    completed the first phase of game developement

--- main.py
+++ main.py
@@ -1,21 +1,61 @@
 import pygame
+import sys
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group() 
+    Player.containers = ( updatable, drawable )
+    Asteroid.containers = ( asteroids, updatable, drawable )
+    AsteroidField.containers = ( updatable )
+    Shot.containers = ( shots, updatable, drawable )
+    asteroid_field = AsteroidField()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.__version__}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+
         screen.fill('black')
+        for players in drawable:
+            players.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+        
 
 
 if __name__ == "__main__":
